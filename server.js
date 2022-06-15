9909d2ca20f55b41d684b9967c1ecf5d6c334390
lovetraindriver <lovetraindriver@gmail.com>
2022-06-15 08:44:21 +0700

    new routes for cameras

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(jsonServer.rewriter({
     "/api/v1/auth/login": "/user",
-    "/api/v1/nodes/": "/servers"
+    "/api/v1/nodes/": "/servers",
+    "/api/v1/nodes/:parent_id/clients": "/cameras?parent_id=:parent_id",
+    "/api/v1/nodes/:parent_id/clients/:id": "/cameras?parent_id=:parent_id,id=:id"
   }))
 server.use(router)
 server.listen(3000, () => {
